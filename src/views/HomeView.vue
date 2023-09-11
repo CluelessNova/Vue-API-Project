4731532b28b122a5a9f5d23385d6186d21f26308
CluelessNova <57334413+CluelessNova@users.noreply.github.com>
2023-09-11 15:02:59 -0400

    Adding mobile responsiveness + cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="about">
+    <h1>This is my Vue.js playgound</h1>
+  </div>
+
+  <div>
+    <h3>What is this for?</h3>
+    <p>I made this to help me understand how Vue.js works.</p>
+    <p>This is also a place where I can mess around with different technologies and to expand my knowledge on components such as API's.</p>
+  </div>
+
+  <div>
+    <h3>What is in here?</h3>
+    <p>A weather API and a movie API is what I started with to understand API calls, endpoints, Vue.js pages, and routers</p>
   </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
-</script>
--- src/views/Movies/Movie.vue
+++ src/views/Movies/Movie.vue
@@ -1,22 +1,29 @@
 <template>
     <h1>Movies</h1>
 
-    <div id="form" class="formGroup field">
+    <div id="inputForm" class="formGroup field">
+        <div id="inputFormLabel" class="inputFormItem">
         <label id="labelField" for="movieSearchField">Enter movie name: </label>
-        <input type="text" placeholder="ex: King Kong" v-model="movieSearch" v-on:keyup.enter="searchMovie" id="inputSearch"
-            name="movieSearchField">
+        </div>
+        <div class="inputFormItem">
+        <input type="text" placeholder="ex: King Kong" v-model="movieSearch" v-on:keyup.enter="searchMovie" id="inputSearch" name="movieSearchField">
+        </div>
+        <div class="inputFormItem">
         <button @click="searchMovie" id="searchButton">Enter</button>
+        </div>
     </div>
 
     <div v-if="responseAvailable == true" id="allMovies">
         <div v-for="movies in movieResults">
-            <div>
-                <h3>{{ movies.title }}</h3>
-                <p>{{ movies.release_date }}</p>
-            </div>
-            <div>
-                <img id="moviePoster" :src="movies.poster_path">
-            </div>
+            <b-card>
+                <b-card-text>
+                    <h3>{{ movies.title }}</h3>
+                    <p>{{ movies.release_date }}</p>
+                </b-card-text>
+                <div>
+                    <img id="moviePoster" :src="movies.poster_path">
+                </div>
+            </b-card>
         </div>
     </div>
 </template>
