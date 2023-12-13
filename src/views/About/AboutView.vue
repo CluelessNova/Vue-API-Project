7b342a3732469bffd2894678c010667809a0e40c
Jake <57334413+CluelessNova@users.noreply.github.com>
2023-12-12 20:01:22 -0500

    Stylize changes and image update

--- src/views/About/AboutView.vue
+++ src/views/About/AboutView.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="allContent">
   <div>
-    <h1 class="text-3xl lg:text-6xl">Who am I?</h1>
+    <h1 class="text-3xl md:text-5xl lg:text-6xl">Who am I?</h1>
   </div>
 
   <div class=" flex flex-column lg:flex-row">
     <div class="col lg:col-6 lg:w-6">
-      <h2 class="aboutme-title text-base lg:text-5xl flex justify-content-center m-2">Get to know me</h2>
+      <h2 class="aboutme-title text-base md:text-xl lg:text-5xl flex justify-content-center m-2">Get to know me</h2>
       <div>
-        <p class="aboutme-description text-sm xl:text-4xl">I am a <strong>software engineer</strong> that works on the <strong>back-end</strong> of web and application development.</p>
-        <p class="aboutme-description text-sm xl:text-4xl">I'm an <strong>Eagle Scout</strong> since 2018 and I volunteer at my home town ambulance agency as an EMT.</p>
-        <p class="aboutme-description text-sm xl:text-4xl">I am open for any opportunity to learn and expand my knowledge and experience. If you'd like to connect through my <a id="linkedIn" href="https://www.linkedin.com/in/jakelevinsky/">LinkedIn</a>, feel free to reach out. </p>
+        <p class="aboutme-description text-sm md:text-xl lg:text-2xl xl:text-4xl">I am a <strong>software engineer</strong> that works on the <strong>back-end</strong> of web and application development.</p>
+        <p class="aboutme-description text-sm md:text-xl lg:text-2xl xl:text-4xl">I'm an <strong>Eagle Scout</strong> since 2018 and I volunteer at my home town ambulance agency as an EMT.</p>
+        <p class="aboutme-description text-sm md:text-xl lg:text-2xl xl:text-4xl">I am open for any opportunity to learn and expand my knowledge and experience. If you'd like to connect through my <a id="linkedIn" target="_blank" href="https://www.linkedin.com/in/jakelevinsky/">LinkedIn</a>, feel free to reach out. </p>
       </div>
     </div>
 
     <div class="col lg:col-6 lg:w-6">
-      <h2 class="aboutme-title text-base lg:text-5xl flex justify-content-center m-2">Skills</h2>
+      <h2 class="aboutme-title text-base md:text-xl lg:text-5xl flex justify-content-center m-2">Skills</h2>
       <div id="skills" class="flex flex-wrap justify-content-center">
-        <div class="aboutme-skills p-3 lg:p-4 text-xs lg:text-2xl" v-for="skill in skills"> {{ skill }} </div>
+        <div class="aboutme-skills p-3 lg:p-4 text-xs lg:text-md xl:text-2xl" v-for="skill in skills"> {{ skill }} </div>
       </div>
     </div>
   </div>
 
-  <h2 class="text-base lg:text-5xl flex justify-content-center mt-5">Timeline of me</h2>
+  <h2 class="text-base md:text-2xl lg:text-5xl flex justify-content-center mt-5">Timeline of me</h2>
   <Timeline :value="events" align="alternate" class="lg:w-8 lg:m-auto">
     <template #content="slotProps">
       <div class="text-xs lg:text-base">
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -3,14 +3,14 @@
     <div id="aboutMe-container">
       <div id="changingName">
         <transition name="fade" mode="out-in">
-        <h1 class="text-4xl lg:text-7xl m-4"><span :key="displayText" style="font-weight: bold;">{{ this.displayText }}</span> Jake Levinsky</h1>
+        <h1 class="text-4xl lg:text-7xl xl:text-7xl m-4"><span :key="displayText" style="font-weight: bold;">{{ this.displayText }}</span> Jake Levinsky</h1>
         </transition>
       </div>
       <h1 class="aboutMe-content" id="myDescription">
-        <p id="myDescription-first" class="text-2xl md:text-4xl xl:text-5xl"></p>
+        <p id="myDescription-first" class="text-2xl md:text-4xl lg:text-6xl xl:text-6xl"></p>
         <br>
         <br>
-        <p id="myDescription-second" class="text-2xl md:text-4xl xl:text-5xl"></p>
+        <p id="myDescription-second" class="text-2xl md:text-4xl lg:text-6xl xl:text-6xl"></p>
       </h1>
     </div>
   </div>
